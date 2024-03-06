34f642e488f32eee92a34f02694f76caf0fe9120
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-06 12:58:58 -0600

    server

---
+++ back/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const server = require("./src/app");
+const { PORT } = process.env;
+
+server.listen(PORT, async () => {
+  console.log("Server raised in port: " + PORT);
+});
---
+++ back/src/app.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+const server = express();
+const router = require("./routes");
+const path = require("path");
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+
+server.use(express.json());
+server.use(morgan("dev"));
+
+server.use("/", router);
+
+module.exports = server;
