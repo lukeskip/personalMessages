b538a32dc987d3e112b5d082b04bcd76ff9915a7
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-07 11:58:57 -0600

    test

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -3,7 +3,7 @@ const requestMessages = require("../controllers/requestMessages");
 const { SENDER } = process.env;
 
 router.post("/", requestMessages);
-router.post("/test", (req, res) => {
+router.get("/test", (req, res) => {
   res.json({ sender: SENDER });
 });
 
