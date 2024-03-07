92a3b56ab9af70ca0a80d9c3e7149826e2ca9300
Cheko Garcia <contacto@chekogarcia.com.mx>
2024-03-07 12:48:43 -0600

    credits

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,6 +6,12 @@
   <main class="container">
       <router-view></router-view>
   </main>
+  <div class="container">
+
+    <footer>
+      Desarrollado por <a href="https://github.com/lukeskip" target="_blank">Cheko García</a> | Powered by OpenAI
+    </footer>
+  </div>
 </template>
 <style> 
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
@@ -21,6 +27,13 @@
   .container{
     max-width: 1200px;
     margin: auto;
+  }
+
+  footer{
+    max-width: 1200px;
+    margin: auto;
+    text-align: center;
+    margin-top: 50px;
   }
 
   .button{
